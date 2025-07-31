000d660f1c6f87c9e716874263d98be93861ac92
Moritz <72256420+moritzc@users.noreply.github.com>
2025-07-31 19:57:09 +0200

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,23 +1,20 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from datetime import datetime
 from .db import Base
 
 class Box(Base):
     __tablename__ = "boxes"
-
-    id = Column(Integer, primary_key=True, index=True)
+    id   = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True, nullable=False)
-
-    # Beziehung zu Items (mit Cascade-Löschung)
     items = relationship("Item", back_populates="box", cascade="all, delete-orphan")
 
 class Item(Base):
     __tablename__ = "items"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, nullable=False)
+    id       = Column(Integer, primary_key=True, index=True)
+    name     = Column(String, index=True, nullable=False)
+    category = Column(String, nullable=True)
     quantity = Column(Integer, default=1)
-
-    # Fremdschlüssel mit ondelete="CASCADE"
-    box_id = Column(Integer, ForeignKey("boxes.id", ondelete="CASCADE"))
-    box = relationship("Box", back_populates="items")
+    last_updated = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    box_id   = Column(Integer, ForeignKey("boxes.id", ondelete="CASCADE"))
+    box      = relationship("Box", back_populates="items")
